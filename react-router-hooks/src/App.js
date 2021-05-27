ef0f4e1da0fde7f86836487c6764754d5540dafe
Andrew TOwnsley <atownsley86@gmail.com>
2021-05-26 23:21:36 -0600

    Film and People views created.  Next is fetching the Ghibli API

--- react-router-hooks/src/App.js
+++ react-router-hooks/src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
+
   return (
-   
-  );
+      <BrowserRouter>
+        <main>
+          <ul className="navbar">
+            <li>
+              <Link to="/">Home</Link>
+              </li>
+            <li>
+              <Link to="/films">Films</Link>
+              </li>
+            <li>
+              <Link to="/people">People</Link>
+              </li>
+          </ul>
+          <Switch>
+            <Route path="/">
+              
+            </Route>
+            <Route path="/films">
+
+            </Route>
+            <Route path="/people">
+
+            </Route>
+          </Switch>
+        </main>
+      </BrowserRouter>
+
+  )
 }
 
 export default App;
---
+++ react-router-hooks/src/views.js/Films.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Films = () => {
+  return (
+    <h1>Films</h1>
+  )
+}
+
+export default Films;
